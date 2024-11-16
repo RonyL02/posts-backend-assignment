ee5643ea1ec07ae465cf91d4f6ce2974280288c2
RonyL02 <280202rony@gmail.com>
2024-11-16 16:36:07 +0200

    create comment model

---
+++ src/models/comment_model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    senderId: {
+        type: Number,
+        required: true
+    },
+    postId: {
+        type: Schema.ObjectId,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+});
+
+const CommentModel = mongoose.model('comments', commentSchema);
+
+module.exports = { CommentModel };
