03ec1e0a14875a552ae5f8794f39452d93820bbe
RonyL02 <280202rony@gmail.com>
2024-11-16 17:49:31 +0200

    add comment routes

---
+++ src/routes/comment_routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/');
+
+router.get('/');
+
+router.put('/');
+router.delete('/:id');
+
+module.exports = router;
