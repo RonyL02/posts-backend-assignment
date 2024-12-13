9bfc5cf810a8445015be5d6c6291ea96b0b7bcae
Shiran Shmuel <94925848+Shiran1212@users.noreply.github.com>
2024-12-13 21:42:41 +0200

    Add base controller

--- src/routes/post_routes.ts
+++ src/routes/post_routes.ts
@@ -1,14 +1,14 @@
-import * as PostController from "../controllers/post_controller";
 import express from "express";
-
+import { PostController } from "../controllers/post_controller";
+const postController=new PostController();
 const PostRouter = express.Router();
 
-PostRouter.get("/", PostController.getAllPosts);
+PostRouter.get("/", postController.find.bind(postController));
 
-PostRouter.get("/:id", PostController.getPostById);
+PostRouter.get("/:id", postController.findById.bind(postController));
 
-PostRouter.put("/:id", PostController.updatePost);
+PostRouter.put("/:id", postController.update.bind(postController));
 
-PostRouter.post("/", PostController.createPost);
+PostRouter.post("/", postController.create.bind(postController));
 
 export { PostRouter };
