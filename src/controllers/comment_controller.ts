c433669f727626ce0cb8d950a9406978d9d13757
Shiran Shmuel <94925848+Shiran1212@users.noreply.github.com>
2024-12-13 22:27:47 +0200

    Unite imports

--- src/controllers/comment_controller.ts
+++ src/controllers/comment_controller.ts
@@ -1,6 +1,5 @@
 import { BaseController } from "./base_controller";
-import { IComment } from "../models/comment_model";
-import { CommentModel } from "../models/comment_model";
+import { IComment,CommentModel } from "../models/comment_model";
 export class CommentController extends BaseController<IComment> {
     constructor() {
         super(CommentModel);
