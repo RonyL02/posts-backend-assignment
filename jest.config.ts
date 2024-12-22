d4a057474b2342c70231ecfa3d6d79d78101cd93
Shiran Shmuel <94925848+Shiran1212@users.noreply.github.com>
2024-12-22 23:21:32 +0200

    Changes of the comments

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,3 @@
-/*@type {import('ts-jest/dist/types').InitialOptionsTsJest} 
-export default {
-    preset: "ts-jest", // שימוש בפריסט של ts-jest לבדיקות ב-TypeScript
-    testEnvironment: "node", // סביבת עבודה מתאימה ל-Node.js
-    roots: ["<rootDir>/tests"], // מיקום תיקיית הבדיקות (ניתן לשנות אם יש תיקייה אחרת)
-    moduleFileExtensions: ["ts", "js"], // סיומות הקבצים ש-Jest יתמוך בהם
-    testMatch: ["**.test.ts"], // התאמת קבצים עם הסיומת test.ts
-   // verbose: true, // הפעלת פלט מפורט של תוצאות הבדיקות
-    globals: {
-      "ts-jest": {
-        isolatedModules: true, // שיפור ביצועים אם לא נעשה שימוש ב-TypeScript מורכב
-      },
-    },
-  };
-  */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
     preset: "ts-jest",
--- src/tests/comments.test.ts
+++ src/tests/comments.test.ts
@@ -4,6 +4,7 @@ import { Express } from "express";
 import { initApp } from "../app";
 import { CommentModel, IComment } from "../models/comment_model";
 import testCommentJson from "./test_comments.json";
+import { StatusCodes } from "http-status-codes";
 
 
 
@@ -14,7 +15,6 @@ const baseUrl = "/comments";
     postId: string;
     content: string;
     _id?: string
-    //לפעמים חובה לפעמים לא
   }
   
 let app: Express;
@@ -34,14 +34,14 @@ afterAll(async () => {
 describe("Comments API Tests", () => {
   test("Get all comments when empty", async () => {
     const response = await request(app).get(baseUrl);
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(StatusCodes.OK);
     expect(response.body.length).toBe(0);
   });
 
   test("Create new comments", async () => {
     for (let comment of testComments) {
       const response = await request(app).post(baseUrl).send(comment);
-      expect(response.statusCode).toBe(201);
+      expect(response.statusCode).toBe(StatusCodes.CREATED);
       expect(response.body.newId).toBeDefined();
         comment._id = response.body.newId;
     }
@@ -50,22 +50,20 @@ describe("Comments API Tests", () => {
   
   test("Get all comments", async () => {
     const response = await request(app).get(baseUrl);
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(StatusCodes.OK);
     expect(response.body.length).toBe(testComments.length);
   });
 
   test("Get comment by ID", async () => {
     const response = await request(app).get(`${baseUrl}/${testComments[0]._id}`);
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(StatusCodes.OK);
     expect(response.body.content).toBe(testComments[0].content);
   });
   
   test("Get comments by post ID", async () => {
-    const postId = testComments[0].postId; 
-    console.log(`${baseUrl}?postId=${postId}`);
+    const postId = testComments[0].postId;
     const response = await request(app).get(`${baseUrl}?postId=${postId}`);
-    console.log(response.body);
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(StatusCodes.OK);
     expect(response.body.length).toBeGreaterThan(0);
     expect(response.body[0].postId).toBe(postId);
   });
@@ -75,7 +73,7 @@ describe("Comments API Tests", () => {
     const response = await request(app)
       .put(`${baseUrl}/${testComments[0]._id}`)
       .send(updatedContent);
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(StatusCodes.OK);
 
     const responseGet = await request(app).get(`${baseUrl}/${testComments[0]._id}`);
     expect(responseGet.body.content).toBe(updatedContent.content);
@@ -83,16 +81,16 @@ describe("Comments API Tests", () => {
 
   test("Delete a comment", async () => {
     const response = await request(app).delete(`${baseUrl}/${testComments[0]._id}`);
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(StatusCodes.OK);
 
     const responseGet = await request(app).get(`${baseUrl}/${testComments[0]._id}`);
-    expect(responseGet.statusCode).toBe(404);
+    expect(responseGet.statusCode).toBe(StatusCodes.NOT_FOUND);
   });
 
   test("Fail to create invalid comment", async () => {
     const invalidComment = { senderId: 3 };
     const response = await request(app).post(baseUrl).send(invalidComment);
-    expect(response.statusCode).toBe(500);
+    expect(response.statusCode).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
   });
   
 
