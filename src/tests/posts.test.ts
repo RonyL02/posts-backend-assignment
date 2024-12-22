7f8a8facd8db93a620ac838470834c2ca9cf1057
Shiran Shmuel <94925848+Shiran1212@users.noreply.github.com>
2024-12-22 23:28:07 +0200

    Changes of the posts

--- src/tests/posts.test.ts
+++ src/tests/posts.test.ts
@@ -4,6 +4,7 @@ import { Express } from "express";
 import { initApp } from "../app";
 import { PostModel } from "../models/post_model";
 import  testPostJson  from "./test_posts.json";
+import { StatusCodes } from "http-status-codes";
 
 type Post = {
     title: string;
@@ -16,22 +17,11 @@ let app: Express;
 const testPosts:Post[]=testPostJson;
 
 const baseUrl = "/posts";
-//const testPosts = require("./test_posts.json");
-// נתונים לדוגמה לפוסטים
-/*const testPosts: { title: string; content: string; author: string; _id?: string }[] = [
-  { title: "First Post",
-     content: "This is the first post",
-      author: "User1" },
-
-  { title: "Second Post",
-     content: "This is the second post",
-      author: "User2" },
-];*/
 
 beforeAll(async () => {
   console.log("Before all tests");
   app = await initApp();
-  await PostModel.deleteMany(); // ננקה את המסד נתונים לפני הריצה
+  await PostModel.deleteMany();
 });
 
 afterAll(async () => {
@@ -42,29 +32,29 @@ afterAll(async () => {
 describe("Posts API Tests", () => {
   test("Get all posts when empty", async () => {
     const response = await request(app).get(baseUrl);
-    expect(response.statusCode).toBe(200);
-    expect(response.body.length).toBe(0); // המסד נתונים ריק
+    expect(response.statusCode).toBe(StatusCodes.OK);
+    expect(response.body.length).toBe(0);
   });
 
   test("Create new posts", async () => {
     for (let post of testPosts) {
         console.log(post);
       const response = await request(app).post(baseUrl).send(post).set('Content-Type', 'application/json');
-      expect(response.statusCode).toBe(201); // סטטוס ליצירה מוצלחת
+      expect(response.statusCode).toBe(StatusCodes.CREATED); 
       expect(response.body.newId).toBeDefined();
-      post["_id"] = response.body.newId; // שמירת מזהה הפוסט לשימוש עתידי
+      post["_id"] = response.body.newId; 
     }
   });
 
   test("Get all posts", async () => {
     const response = await request(app).get(baseUrl);
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(StatusCodes.OK);
     expect(response.body.length).toBe(testPosts.length);
   });
 
   test("Get post by ID", async () => {
     const response = await request(app).get(`${baseUrl}/${testPosts[0]._id}`);
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(StatusCodes.OK);
     expect(response.body.title).toBe(testPosts[0].title);
     expect(response.body.content).toBe(testPosts[0].content);
   });
@@ -74,7 +64,7 @@ describe("Posts API Tests", () => {
     const response = await request(app)
       .put(`${baseUrl}/${testPosts[0]._id}`)
       .send(updatedData);
-    expect(response.statusCode).toBe(200);
+    expect(response.statusCode).toBe(StatusCodes.OK);
 
     const responseGet = await request(app).get(`${baseUrl}/${testPosts[0]._id}`);
     expect(responseGet.body.title).toBe(updatedData.title);
@@ -82,8 +72,8 @@ describe("Posts API Tests", () => {
   });
 
   test("Fail to create invalid post", async () => {
-    const invalidPost = { author: "UserWithoutTitle" }; // נתונים לא מלאים
+    const invalidPost = { author: "UserWithoutTitle" }; 
     const response = await request(app).post(baseUrl).send(invalidPost);
-    expect(response.statusCode).toBe(500); // בדיקה שהבקשה נדחית
+    expect(response.statusCode).toBe(StatusCodes.INTERNAL_SERVER_ERROR); 
   });
 });
